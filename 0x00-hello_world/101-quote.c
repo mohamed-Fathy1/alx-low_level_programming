fa014e20e8a0b4f6f44b9edbc263980509317bd6
mohamed-Fathy1 <mohamedfathysaied18@gmail.com>
2023-02-16 18:47:01 -0800

    Update 0x00-hello_world folder

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -7,6 +7,7 @@
 */
 int main(void)
 {
-	fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stout);
-return (1);	
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",
+			sizeof("and that piece of art is useful\" - Dora Korpar, 2015-10-19"));
+	return (1);
 }
