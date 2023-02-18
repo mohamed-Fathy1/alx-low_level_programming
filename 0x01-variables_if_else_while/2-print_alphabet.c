e89b76b803b2ee7070d8dc01d92e2ed2039358d2
mohamed-Fathy1 <mohamedfathysaied18@gmail.com>
2023-02-17 18:46:30 -0800

    Update 0x01-variables_if_else_while folder

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -6,16 +6,14 @@
 */
 int main(void)
 {
-	int n;
+	int c;
 
-	srand(time(0));
-	n = rand() - RAND_MAX / 2;
-	/* your code goes there */
-	if (n > 0)
-		printf("%d is positive\n", n);
-	else if (n == 0)
-		printf("%d is zero\n", n);
-	else
-		printf("%d is negative\n", n);
+	c - 'a';
+	while (c <= 'z')
+	{
+		putchar(c);
+		c++;
+	}
+	putchar("\n")
 	return (0);
 }
