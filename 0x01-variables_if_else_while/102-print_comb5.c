2446e815e9d7a75b17530eaa02c1285117f4453d
mohamed-Fathy1 <mohamedfathysaied18@gmail.com>
2023-02-17 23:48:03 -0800

    Update 0x00-hello_world folder

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -18,14 +18,14 @@ int main(void)
 		{
 			for (k = 0; k <= 9; k++)
 			{
-				for (m = 0; m <= 9; m++)
+				for (m = j + 1; m <= 9; m++)
 				{
 					putchar('0' + i);
 					putchar('0' + j);
 					putchar(' ');
 					putchar('0' + k);
 					putchar('0' + m);
-					if (i < 9 || j < 9 || k < 9 || m < 9)
+					if (i < 9 || j < 8 || k < 9 || m < 9)
 					{
 						putchar(',');
 						putchar(' ');
