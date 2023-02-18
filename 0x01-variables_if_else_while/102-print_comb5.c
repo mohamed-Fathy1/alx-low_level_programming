af9599c72b9c16636214c904818e567c29cc9b1a
mohamed-Fathy1 <mohamedfathysaied18@gmail.com>
2023-02-17 22:47:21 -0800

    Update 0x00-hello_world folder

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+/**
+* main - Entry point
+*
+* Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+	int c;
+	int i;
+
+	c = 0;
+	while (c <= 9)
+	{
+		putchar('0' + c);
+		putchar('0' + i);
+		if (c < 9 || i < 9)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+		if (i >= 9)
+		{
+			i = 0;
+			c++;
+		}
+		else
+		{
+			i++;
+		}
+	}
+	putchar('\n');
+	return (0);
+}
