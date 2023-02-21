8c55d70962d45da620ffb427bb9a032c9861c807
mohamed-Fathy1 <mohamedfathysaied18@gmail.com>
2023-02-21 08:46:59 -0800

    Add void print_alphabet_x10 function

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - check the code
+ *
+ * Return: No return type (void).
+ */
+void print_alphabet_x10(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		print_alphabet();
+	}
+	_putchar('\n');
+}
