71240434be4bbed1763aca8578e30d7e830270fd
mohamed-Fathy1 <mohamedfathysaied18@gmail.com>
2023-02-21 13:10:16 -0800

    Add 98 function

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,46 @@
+#include "main.h"
+#include "1-alphabet.c"
+#include <stdio.h>
+/**
+ * print_to_98 - check the code
+ *
+ * @n: first parameter
+ *
+ * Return: No return type (void).
+ */
+void print_to_98(int n)
+{
+	int i;
+
+	if (n <= 98)
+	{
+		for (i = n; i <= 98; i++)
+		{
+			if (i < 98)
+			{
+				printf("%d, ", i);
+			}
+			else
+			{
+				printf("%d", i);
+			}
+
+		}
+	}
+	else
+	{
+		for (i = n; i > 98; i--)
+		{
+			if (i > 98)
+			{
+				printf("%d, ", i);
+			}
+			else
+			{
+				printf("%d", i);
+			}
+
+		}
+	}
+	printf("\n");
+}
