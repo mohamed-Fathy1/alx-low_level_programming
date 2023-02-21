1028f7ccf9d7eb0ee1c0018b8710060de5b6ee4f
mohamed-Fathy1 <mohamedfathysaied18@gmail.com>
2023-02-21 12:15:03 -0800

    Add add function

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include "1-alphabet.c"
+#include <stdio.h>
+/**
+ * add - check the code
+ *
+ * @a : first parameter
+ * @b: second parameter
+ *
+ * Return: No return type (void).
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
