7f0e9e38dc24ebcab2a9171b0a954430b71bc18c
mohamed-Fathy1 <mohamedfathysaied18@gmail.com>
2023-02-21 09:06:03 -0800

    Add isLower function

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,14 @@
+#include <ctype.h>
+
+/*
+ * _islower - check if typed input is lowercase
+ *
+ * Return : 1 if lowercase else return 0
+ */
+int _islower(int c)
+{
+	if (islower(c))
+		return (1);
+	else
+		return (0);
+}
