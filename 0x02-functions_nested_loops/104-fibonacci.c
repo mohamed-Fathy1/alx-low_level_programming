48f2830db185dae20b13808d8a8ba9b2335358aa
mohamed-Fathy1 <mohamedfathysaied18@gmail.com>
2023-02-21 14:34:55 -0800

    Update folder

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -17,9 +17,9 @@ printf("%lu, ", sum);
 fib1 = fib2;
 fib2 = sum;
 }
-fibl_half1 = fib1 / 10000000000;
+fib1_half1 = fib1 / 10000000000;
 fib2_half1 = fib2 / 10000000000;
-fibl_half2 = fib1 % 10000000000;
+fib1_half2 = fib1 % 10000000000;
 fib2_half2 = fib2 % 10000000000;
 for (count = 93; count < 99; count++)
 {
