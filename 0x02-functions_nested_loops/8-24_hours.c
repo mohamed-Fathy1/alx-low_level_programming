23f3912e6d54cd16acf52346a7fc14115fe46aa9
mohamed-Fathy1 <mohamedfathysaied18@gmail.com>
2023-02-21 10:50:25 -0800

    Add clock counter function

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,5 +1,6 @@
 #include "main.h"
 #include "1-alphabet.c"
+#include <stdio.h>
 /**
  * jack_bauer - check the code
  *
@@ -11,8 +12,12 @@ void jack_bauer(void)
 
 	for (i = 0; i <= 2; i++)
 	{
-		for (j = 0; j <= 3; j++)
+		for (j = 0; j <= 9; j++)
 		{
+		if (i == 2 && j > 3)
+		{
+			break;
+		}
 			for (k = 0; k <= 5; k++)
 			{
 			for (m = 0; m <= 9; m++)
