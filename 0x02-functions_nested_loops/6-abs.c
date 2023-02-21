d2b9f43e37db8dbd1e9c59e249962e748f9377c6
mohamed-Fathy1 <mohamedfathysaied18@gmail.com>
2023-02-21 09:52:03 -0800

    Add abs function

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,13 @@
+#include <stdlib.h>
+
+/**
+ * _abs - check if typed input is lowercase
+ *
+ * @c: takes parameter c type of int and ckeck of its lowercase
+ *
+ * Return: return 1 if lowercase else return 0
+ */
+int _abs(int c)
+{
+	return (abs(c));
+}
