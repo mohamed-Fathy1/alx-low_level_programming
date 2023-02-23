7e48cb87cebf2ced2fc3ac9a6a4944798b7a047d
mohamed-Fathy1 <mohamedfathysaied18@gmail.com>
2023-02-23 07:02:55 -0800

    Add task 5

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,29 @@
+#include "main.h"
+/**
+ * more_numbers - check the code
+ *
+ * Return: Always 0.
+ */
+void more_numbers(void)
+{
+	int i, j, a, b;
+
+	for (i = 1; i <= 10; i++)
+	{
+	for (j = 0; j <= 14; j++)
+	{
+		if(j < 10)
+		{
+			_putchar('0' + j);
+		}
+		else
+		{
+			a = j / 10;
+			b = j % 10;
+			_putchar('0' + a);
+			_putchar('0' + b);
+		}
+	}
+	_putchar('\n');
+	}
+}
