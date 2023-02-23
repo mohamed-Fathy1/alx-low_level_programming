855254615f8f9a5bc7b75093f4087322c7da24c8
mohamed-Fathy1 <mohamedfathysaied18@gmail.com>
2023-02-23 08:37:50 -0800

    Add task 7

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,29 @@
+#include "main.h"
+/**
+ * print_diagonal - check the code
+ *
+ * @n: first argument
+ *
+ * Return: Always 0.
+ */
+void print_diagonal(int n)
+{
+	int i, j;
+
+	if (n > 0)
+	{
+		for (i = 1; i <= n; i++)
+		{
+			for (j = 1; j < i; j++)
+			{
+				_putchar(' ');
+			}
+			_putchar('\\');
+			_putchar('\n');
+		}
+	}
+	else
+	{
+		_putchar('\n');
+	}
+}
