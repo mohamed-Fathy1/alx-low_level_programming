305fc01b281ba297649056ab37d899fe884356b7
mohamed-Fathy1 <mohamedfathysaied18@gmail.com>
2023-02-28 11:11:33 -0800

    Add task 4

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -12,11 +12,11 @@ void print_rev(char *s)
 	int i, j;
 	int len = strlen(s);
 
-	for (i = 0; i < len; i++)
+	for (i = 0; i < len - 1; i++)
 	{
 		s++;
 	}
-	for (j = len; j >= 0; j--)
+	for (j = len - 1; j >= 0; j--)
 	{
 		_putchar(*s);
 		s--;
