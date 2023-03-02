2f27a0e90119f93410c4b68fc634ec8850bce667
mohamed-Fathy1 <mohamedfathysaied18@gmail.com>
2023-03-02 07:26:27 -0800

    Add task 3

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,12 @@
+#include "main.h"
+#include <string.h>
+/**
+ * _strcmp - prints half of a string
+ * @s1: input
+ * @s2: input
+ * Return: dest
+ */
+int _strcmp(char *s1, char *s2)
+{
+	return (strcmp(s1, s2));
+}
