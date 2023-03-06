8900f6b816554fdf05c373c4634c441505708458
mohamed-Fathy1 <mohamedfathysaied18@gmail.com>
2023-03-06 15:30:57 -0800

    Add task 4

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,22 @@
+/**
+ * _strpbrk - main function
+ *
+ * @s: first input
+ * @accept: second input
+ *
+ * Return: char pointer
+ */
+char *_strpbrk(char *s, char *accept)
+{
+	unsigned int i, j;
+
+	for (i = 0; s[i] >= '\0'; i++)
+	{
+		for (j = 0; accept[j] >= '\0'; j++)
+		{
+			if (s[i] == accept[j])
+				return (&s[i]);
+		}
+	}
+	return (0);
+}
