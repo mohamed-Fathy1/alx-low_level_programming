0fbe671b2cf33774d0c7d4fa41d6bfef9da075a1
mohamed-Fathy1 <mohamedfathysaied18@gmail.com>
2023-03-06 14:42:50 -0800

    Add task 2

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -1,4 +1,5 @@
 #include <string.h>
+#include <stdio.h>
 /**
  * _strchr - main function
  *
@@ -11,15 +12,13 @@ char *_strchr(char *s, char c)
 {
 	unsigned int i;
 	unsigned int len = strlen(s);
-	char *p = NULL;
 
 	for (i = 0; i < len; i++)
 	{
 		if (s[i] == c)
 		{
-			p = &s[i];
-			break;
+			return (&s[i]);
 		}
 	}
-	return (p);
+	return (0);
 }
