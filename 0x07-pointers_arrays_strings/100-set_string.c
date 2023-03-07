975d94057c6c08f7dfe5b53e525c905db8ce6247
mohamed-Fathy1 <mohamedfathysaied18@gmail.com>
2023-03-06 17:18:04 -0800

    Add task 100

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,12 @@
+/**
+ * set_string - main function
+ *
+ * @s: first input
+ * @to: first input
+ *
+ * Return: void
+ */
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -15,3 +15,5 @@ char *_strstr(char *haystack, char *needle);
 int _putchar(char);
 
 void print_diagsums(int *a, int size);
+
+void set_string(char **s, char *to);
