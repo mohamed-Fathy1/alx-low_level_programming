c007af40ce8b1a2deb45aec2b61b5b79777be908
mohamed-Fathy1 <mohamedfathysaied18@gmail.com>
2023-03-08 10:58:31 -0800

    Add task 1

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * _print_rev_recursion - function description
+ *
+ * @s: input
+ */
+void _print_rev_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		return;
+	}
+	_print_rev_recursion(s + 1);
+	_putchar(*s);
+}
