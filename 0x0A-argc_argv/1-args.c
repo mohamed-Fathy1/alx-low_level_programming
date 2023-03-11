b55733ceaa8ab3fb0920fd30895d1f68fd88b005
mohamed-Fathy1 <mohamedfathysaied18@gmail.com>
2023-03-10 17:43:00 -0800

    add task 0

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - check the code
+ *
+ * @argv: input
+ * @argc: input
+ * Return: Always 0.
+ */
+int main(int argc, char *argv[])
+{
+	(void) argv;
+	printf("%d\n",argc - 1);
+	return (0);
+}
