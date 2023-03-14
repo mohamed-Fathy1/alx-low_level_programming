c588722890129e132916bca040942108fa4529b8
mohamed-Fathy1 <mohamedfathysaied18@gmail.com>
2023-03-14 15:23:15 -0700

    Add task 1

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,29 @@
+#include <stdlib.h>
+/**
+ * _strdup- main fuction
+ * @str: input
+ * Return: char
+ */
+char *_strdup(char *str)
+{
+	unsigned int i;
+	unsigned int len = 0;
+	char *p;
+
+	if (str == NULL)
+		return (NULL);
+
+	while (str[len] != '\0')
+	{
+		len++;
+	}
+
+	p = (char *)malloc((len + 1) * sizeof(*str));
+
+	for (i = 0; i < len; i++)
+	{
+		p[i] = str[i];
+	}
+/*	p[len + 1] = '\0';*/
+	return (p);
+}
