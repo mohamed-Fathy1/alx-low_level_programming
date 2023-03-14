e88657115737d835c62c50e786a41514572fb652
mohamed-Fathy1 <mohamedfathysaied18@gmail.com>
2023-03-14 14:46:13 -0700

    Add task 0

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -10,7 +10,7 @@ char *create_array(unsigned int size, char c)
 	unsigned int i;
 	char *p = (char *)malloc(size);
 
-	if (p == NULL)
+	if (size == 0)
 		return (NULL);
 	for (i = 0; i < size; i++)
 	{
