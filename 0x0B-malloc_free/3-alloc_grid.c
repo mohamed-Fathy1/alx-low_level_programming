e6e92789f3db8272787354f29924464c53dabd5a
mohamed-Fathy1 <mohamedfathysaied18@gmail.com>
2023-03-14 16:36:14 -0700

    Add task 3

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,34 @@
+#include <stdlib.h>
+#include <string.h>
+/**
+ * **alloc_grid - main fuction
+ * @width: input
+ * @height: input
+ * Return: int
+ */
+int **alloc_grid(int width, int height)
+{
+	int  **p;
+	int i, j;
+
+	if (width <= 0 || height <= 0)
+	{
+		return (NULL);
+	}
+
+	p = (int **)malloc(height * sizeof(int **));
+	for (i = 0; i < height; i++)
+		p[i] = malloc(width * sizeof(int *));
+
+	if (p == NULL)
+		return (NULL);
+
+	for (i = 0; i < height; i++)
+	{
+		for (j = 0; j < width; j++)
+		{
+			p[i][j] = 0;
+		}
+	}
+	return (p);
+}
