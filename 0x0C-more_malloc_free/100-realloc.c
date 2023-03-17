1c12dabed13a1ecf2e90064e1fa21077926449a2
mohamed-Fathy1 <mohamedfathysaied18@gmail.com>
2023-03-17 03:48:15 -0700

    Add task 100

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -24,7 +24,7 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 	}
 
 	if (ptr == NULL)
-		return (malloc(new_size));
+		return (p);
 
 	if (ptr != NULL && new_size == 0)
 	{
