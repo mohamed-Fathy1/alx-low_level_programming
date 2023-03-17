08efca0abbd0fa45636a4a4dd72063db915f1307
mohamed-Fathy1 <mohamedfathysaied18@gmail.com>
2023-03-16 18:21:14 -0700

    Add task 1

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -17,7 +17,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	if (s2 == NULL)
 		s2 = "";
 
-	p = malloc((strlen(s1)) * sizeof(char));
+	p = malloc((strlen(s1) + 1) * sizeof(char));
 
 	if (!p)
 		return (NULL);
@@ -25,9 +25,9 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 		p[i] = s1[i];
 
 	if (n < strlen(s2))
-		p = realloc(p, (strlen(s1) + n) * sizeof(char));
+		p = realloc(p, (strlen(s1) + n + 1) * sizeof(char));
 	else
-		p = realloc(p, (strlen(s1) + strlen(s2)) * sizeof(char));
+		p = realloc(p, (strlen(s1) + strlen(s2) + 1) * sizeof(char));
 
 	if (!p)
 		return (NULL);
