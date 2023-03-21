c0f9a9c1e4cccf56ba5ee82819f0a2184b224571
mohamed-Fathy1 <mohamedfathysaied18@gmail.com>
2023-03-21 09:55:23 -0700

    Add task 4

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -11,19 +11,37 @@
 	 * Return: pointer to the new dog (Success), NULL otherwise
 	 */
 dog_t *new_dog(char *name, float age, char *owner)
-{
-	dog_t *dog;
-
-	dog = malloc(sizeof(dog_t));
-	if (dog == NULL)
-		return (NULL);
-	if (name == NULL || owner == NULL)
 	{
-		free (dog);
-		return (NULL);
+		dog_t *dog;
+		int len1, len2;
+
+
+		len1 = _strlen(name);
+		len2 = _strlen(owner);
+
+
+		dog = malloc(sizeof(dog_t));
+		if (dog == NULL)
+			return (NULL);
+
+
+		dog->name = malloc(sizeof(char) * (len1 + 1));
+		if (dog->name == NULL)
+		{
+			free(dog);
+			return (NULL);
+		}
+		dog->owner = malloc(sizeof(char) * (len2 + 1));
+		if (dog->owner == NULL)
+		{
+			free(dog);
+			free(dog->name);
+			return (NULL);
+		}
+		_strcpy(dog->name, name);
+		_strcpy(dog->owner, owner);
+		dog->age = age;
+
+
+		return (dog);
 	}
-	dog->name = name;
-	dog->age = age;
-	dog->owner = owner;
-	return (dog);
-}
