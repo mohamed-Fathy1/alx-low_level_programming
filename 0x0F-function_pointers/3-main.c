7c22f816b0bdc75fd93632ae3ef5ad4ed2813c30
mohamed-Fathy1 <mohamedfathysaied18@gmail.com>
2023-03-22 19:34:53 -0700

    Add task 3

---
+++ 0x0F-function_pointers/3-main.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "3-calc.h"
+
+/**
+ * main - check the code
+ * @argc: input
+ * @argv: input
+ * Return: Always 0.
+ */
+int main(int argc, char *argv[])
+{
+	int a, b;
+	char *opre;
+	if (argc != 4)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+	a = atoi(argv[1]);
+	opre = argv[2];
+	b = atoi(argv[3]);
+
+	if (*opre != '/' && *opre != '+' && *opre != '%'
+&& *opre != '-' && *opre != '*')
+	{
+		printf("Error\n");
+		exit(99);
+	}
+	if ((*opre == '/' || *opre == '%') && (b == 0))
+	{
+		printf("Error\n");
+		exit(100);
+	}
+	printf("%d\n", get_op_func(opre)(a, b));
+	return (0);
+}
