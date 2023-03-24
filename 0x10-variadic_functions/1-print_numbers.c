8dc193afede899c91850edd3c3d51eb0d002cb23
mohamed-Fathy1 <mohamedfathysaied18@gmail.com>
2023-03-23 18:25:05 -0700

    Add task 1

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -11,10 +11,6 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	va_list ap;
 	unsigned int i = 0;
 
-	if (n == 0)
-	{
-		exit(0);
-	}
 	va_start(ap, n);
 
 	for (i = 0; i < n; i++)
