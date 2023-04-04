a79e7f2c4e5965987a9c9d249d563d2738304225
mohamed-Fathy1 <mohamedfathysaied18@gmail.com>
2023-04-04 16:45:55 -0700

    Add task 100

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -3,12 +3,10 @@
 void first(void) __attribute__ ((constructor));
 
 /**
- * first - prints a sentence before the main
- * function is executed
+ * first - function
  */
 void first(void)
 {
 	printf("You're beat! and yet, you must allow,\n");
 	printf("I bore my house upon my back!\n");
 }
-
