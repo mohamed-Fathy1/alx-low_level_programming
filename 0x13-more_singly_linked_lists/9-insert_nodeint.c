75e2124674f3fa14ca746290b038c09a03d13f7d
mohamed-Fathy1 <mohamedfathysaied18@gmail.com>
2023-04-05 14:38:14 -0700

    Add task 9

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -20,6 +20,8 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		return (NULL);
 	}
 	current = *head;
+	if (!current)
+		return (NULL);
 	ptr = malloc(sizeof(listint_t));
 	if (!ptr)
 		return (NULL);
