fdd317f8db775c2584fbc5bcb3424ae186a9ee99
mohamed-Fathy1 <mohamedfathysaied18@gmail.com>
2023-04-04 19:40:10 -0700

    Add task 7

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -22,6 +22,8 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 		for (i = 0; i < index; i++)
 		{
 			node = node->next;
+			if (node == NULL)
+				return NULL;
 		}
 	}
 	return (node);
