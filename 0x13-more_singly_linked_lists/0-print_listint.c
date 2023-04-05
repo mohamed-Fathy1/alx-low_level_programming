23aaddf7307c0f84a996f4889da8451daf838673
mohamed-Fathy1 <mohamedfathysaied18@gmail.com>
2023-04-04 17:52:47 -0700

    Add task 0

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * print_listint - function
+ * @h: input
+ * Return: int
+ */
+size_t print_listint(const listint_t *h)
+{
+	int i = 0;
+
+	while (h)
+	{
+		printf("%d\n", h->n);
+		h = h->next;
+		i++;
+	}
+	return (i);
+}
