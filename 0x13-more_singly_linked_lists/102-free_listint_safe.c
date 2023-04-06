b9324ff0b2aac788d6e3236baf7c7a368b095002
mohamed-Fathy1 <mohamedfathysaied18@gmail.com>
2023-04-05 19:24:48 -0700

    Add task 102

---
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include "lists.h"
+#include <stdlib.h>
+
+/**
+ * free_listint_safe - function
+ * @h: input
+ * Return: size_t
+ */
+size_t free_listint_safe(listint_t **h)
+{
+	listint_t *ptr;
+	size_t lenght = 0;
+	int i;
+
+	if (!h || !*h)
+		return (0);
+
+	while (*h != NULL)
+	{
+		i = *h - (*h)->next;
+		if (i <= 0)
+		{
+			free(*h);
+			*h = NULL;
+			len++;
+			break;
+		}
+		else
+		{
+			ptr = (*h)->next;
+			free(*h);
+			*h = ptr;
+			lenght++;
+		}
+	}
+	*h = NULL;
+	return (lenght);
+}
+
