6487dd035828642e94c1c637e80c906a35e48363
mohamed-Fathy1 <60184431+mohamed-Fathy1@users.noreply.github.com>
2023-04-10 00:19:15 +0200

    Update 2-get_bit.c

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -10,15 +10,10 @@
  */
 int get_bits(unsigned long int n, unsigned int index, unsigned int i)
 {
-	if (index < i)
-		return (-1);
-
 	if (index == i)
 		return (n & 1);
 	if (n > 1)
 		get_bits(n >> 1, index, i + 1);
-	else
-		return (-1);
 	return (get_bits(n >> 1, index, i + 1));
 }
 /**
@@ -29,6 +24,8 @@ int get_bits(unsigned long int n, unsigned int index, unsigned int i)
  */
 int get_bit(unsigned long int n, unsigned int index)
 {
+	if (index > 63)
+		return (-1);
 	return (get_bits(n, index, 0));
 }
 
