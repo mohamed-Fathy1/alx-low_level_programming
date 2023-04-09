e5168bdf1dd08514e4a0bcfa14c2e9273cdc61bb
mohamed-Fathy1 <mohamedfathysaied18@gmail.com>
2023-04-09 14:43:44 -0700

    Add task 2

---
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * get_bits - function
+ * @n: input
+ * @index: input
+ * @i: input
+ * Return: int
+ */
+int get_bits(unsigned long int n, unsigned int index, unsigned int i)
+{
+	if (index < i)
+		return (-1);
+	if (index == i)
+		return (n & 1);
+	if (n > 1)
+		get_bits(n >> 1, index, i + 1);
+	return (get_bits(n >> 1, index, i + 1));
+}
+/**
+ * get_bit - function
+ * @n: input
+ * @index: input
+ * Return: int
+ */
+int get_bit(unsigned long int n, unsigned int index)
+{
+	return (get_bits(n, index, 0));
+}
+
