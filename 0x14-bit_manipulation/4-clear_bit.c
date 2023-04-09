0d1e2f89f2b92b9d11d572ca9f8e05769edbe214
mohamed-Fathy1 <mohamedfathysaied18@gmail.com>
2023-04-09 15:46:31 -0700

    Add task 4

--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -8,10 +8,11 @@
  * @index: input
  * Return: int
  */
-int clear_bit(unsigned long int *n, unsigned int index) {
-    if (index >= sizeof(unsigned long int) * 8) {
-        return -1;
-    }
-    *n &= ~(1 << index);
-    return 0;
+int clear_bit(unsigned long int *n, unsigned int index)
+{
+	if (index >= sizeof(unsigned long int) * 8)
+		return (-1);
+
+	*n &= ~(1 << index);
+	return (1);
 }
