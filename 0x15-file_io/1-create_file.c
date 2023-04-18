6d4cea9c4ec883cb59a4a4bd41f2219b12bc93f8
mohamed-Fathy1 <mohamedfathysaied18@gmail.com>
2023-04-18 02:06:18 -0700

    Add task 1

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -14,7 +14,8 @@ int create_file(const char *filename, char *text_content)
 {
 	int fd, len = 0;
 
-	len = strlen(text_content);
+	if (text_content)
+		len = strlen(text_content);
 
 	if (filename == NULL)
 		return (-1);
