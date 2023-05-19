39020ec4ff9401927bbc7eee77c146f47be9ccfe
mohamed-Fathy1 <mohamedfathysaied18@gmail.com>
2023-05-19 00:50:45 -0700

    Add task 7

--- 0x17-doubly_linked_lists/7-main.c
+++ 0x17-doubly_linked_lists/7-main.c
@@ -13,17 +13,12 @@ int main(void)
     dlistint_t *head;
 
     head = NULL;
-    add_dnodeint_end(&head, -0);
-    add_dnodeint_end(&head, -1);
-    add_dnodeint_end(&head, -2);
-    add_dnodeint_end(&head, -3);
-    add_dnodeint_end(&head, -4);
-    add_dnodeint_end(&head, -98);
-    add_dnodeint_end(&head, -7);
-    add_dnodeint_end(&head, -1);
+    add_dnodeint_end(&head, 0);
+    add_dnodeint_end(&head, 1);
+    add_dnodeint_end(&head, 2);
     print_dlistint(head);
     printf("-----------------\n");
-    insert_dnodeint_at_index(&head, 9, -4096);
+    insert_dnodeint_at_index(&head, 4, 4096);
     print_dlistint(head);
     free_dlistint(head);
     head = NULL;
