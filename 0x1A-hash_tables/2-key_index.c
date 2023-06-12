941d0ccf7525f5008f1aa4f0dc8bb97a608cc615
mohamed-Fathy1 <mohamedfathysaied18@gmail.com>
2023-06-11 19:11:00 -0700

    Add task 0

---
+++ 0x1A-hash_tables/2-key_index.c
@@ -0,0 +1,15 @@
+#include "./hash_tables.h"
+
+/**
+ * key_index - function
+ * @key: argument
+ * @size: argument
+ * Return: unsigned int
+ */
+unsigned long int key_index(const unsigned char *key, unsigned long int size)
+{
+unsigned int hash_number = hash_djb2(key);
+
+(void)(size);
+return (hash_number);
+}
