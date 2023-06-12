a35f0e0fc181f61ccea30b33b6cf17807f5cee8e
mohamed-Fathy1 <mohamedfathysaied18@gmail.com>
2023-06-11 18:20:35 -0700

    Add task 0

---
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -0,0 +1,15 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_create - function
+ * @size: argument
+ * Return: hash_table_t
+ */
+hash_table_t *hash_table_create(unsigned long int size)
+{
+hash_table_t *table = malloc(sizeof(hash_table_t) * size);
+if (!table)
+return (NULL);
+return (table);
+}
+
