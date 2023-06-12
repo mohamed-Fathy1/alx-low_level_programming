fab3aecbca114069edd7be146887ec01406c3493
mohamed-Fathy1 <mohamedfathysaied18@gmail.com>
2023-06-11 18:29:37 -0700

    Add task 0

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -7,7 +7,7 @@
  */
 hash_table_t *hash_table_create(unsigned long int size)
 {
-hash_table_t *table = malloc(sizeof(hash_table_t) * size);
+hash_table_t *table = malloc(sizeof(hash_node_t) * size);
 if (!table)
 return (NULL);
 return (table);
