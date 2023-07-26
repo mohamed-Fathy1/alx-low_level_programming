1a3d0df4babe1591f3f54e290db46e91f007b334
mohamed-Fathy1 <mohamedfathysaied18@gmail.com>
2023-07-27 01:13:20 +0300

    Add task 5

---
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+"""  island perimeter Technical interview preparation """
+
+
+def island_perimeter(grid):
+    """
+     returns the perimeter of the island described in grid:
+
+     Arg(grid):
+        grid is a list of list of integers:
+        0 represents a water zone
+        1 represents a land zone
+        One cell is a square with side length 1
+    """
+    count = 0
+    num = 0
+    for i in grid:
+        num1 = 0
+        for j in i:
+            if j == 1:
+                try:
+                    if grid[num - 1][num1] == 0:
+                        count += 1
+                    if grid[num + 1][num1] == 0:
+                        count += 1
+                    if grid[num][num1 + 1] == 0:
+                        count += 1
+                    if grid[num][num1 - 1] == 0:
+                        count += 1
+                except IndexError:
+                    count += 1
+            num1 += 1
+        num += 1
+
+    return count
